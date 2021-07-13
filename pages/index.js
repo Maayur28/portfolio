0fa9adedd61d9bee2705736c9bab9a64fdd0f48b
Mayur <mayuragarwal2812@gmail.com>
2021-07-13 06:45:45 +0530

    removed refresh because accessibility issue

--- pages/index.js
+++ pages/index.js
@@ -196,7 +196,6 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta charSet="utf-8" />
-        <meta httpEquiv="refresh" content="30"/>
         <link rel="icon" href="/favicon.ico" />
         <title>
           {router.length > 1
